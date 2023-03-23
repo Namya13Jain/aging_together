6f9fbb0f54b7e56db8ea66ee1c0337588e73d286
Namya13Jain <100767035+Namya13Jain@users.noreply.github.com>
2023-03-23 09:03:57 +0530

    companion

--- aging_together/lib/screens/companion/info1.dart
+++ aging_together/lib/screens/companion/info1.dart
@@ -25,75 +25,79 @@ class _ProvideInfo extends State<info1> {
         body: Container(
             padding:
                 const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("What's Your Gender",
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 35, 34, 34),
-                          fontSize: 17,
-                          fontWeight: FontWeight.w600)),
-                  SizedBox(height: 10.0),
-                  Row(
-                    children: [
-                      Checkbox(value: false, onChanged: (bool? newValue) {}),
-                      Text("Male",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 35, 34, 34),
-                              fontSize: 12,
-                              fontWeight: FontWeight.w600)),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Checkbox(value: false, onChanged: (bool? newValue) {}),
-                      SizedBox(height: 50),
-                      Text("Female",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 35, 34, 34),
-                              fontSize: 12,
-                              fontWeight: FontWeight.w600)),
-                    ],
-                  ),
-                  Text("What's Your Age",
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 35, 34, 34),
-                          fontSize: 17,
-                          fontWeight: FontWeight.w600)),
-                  TextField(
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          hintText: "Enter Your Age",
-                          contentPadding:
-                              const EdgeInsets.only(bottom: 5.0, top: 12.5),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ))),
-                  Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: Center(
-                          child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            primary: Color.fromRGBO(8, 143, 143, 1)),
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => info2()));
-                        },
-                        child: Container(
-                          width: double.infinity,
-                          child: Center(
-                            child: Text(
-                              'Next Step',
-                              style: TextStyle(fontSize: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("What's Your Gender",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 35, 34, 34),
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600)),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Checkbox(value: false, onChanged: (bool? newValue) {}),
+                        Text("Male",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 35, 34, 34),
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600)),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(value: false, onChanged: (bool? newValue) {}),
+                        SizedBox(height: 50),
+                        Text("Female",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 35, 34, 34),
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600)),
+                      ],
+                    ),
+                    Text("What's Your Age",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 35, 34, 34),
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600)),
+                    TextField(
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: "Enter Your Age",
+                            contentPadding:
+                                const EdgeInsets.only(bottom: 5.0, top: 12.5),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ))),
+                    Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: Center(
+                            child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              primary: Color.fromRGBO(8, 143, 143, 1)),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => info2()));
+                          },
+                          child: Container(
+                            width: double.infinity,
+                            child: Center(
+                              child: Text(
+                                'Next Step',
+                                style: TextStyle(fontSize: 20),
+                              ),
                             ),
                           ),
-                        ),
-                      ))),
-                ])));
+                        ))),
+                  ]),
+            )));
   }
 }
--- aging_together/lib/screens/companion/info2.dart
+++ aging_together/lib/screens/companion/info2.dart
@@ -24,64 +24,68 @@ class _MoreInfo extends State<info2> {
         body: Container(
             padding:
                 const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Tell Us About Yourself",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 35, 34, 34),
-                        fontSize: 17,
-                        fontWeight: FontWeight.w600)),
-                TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: "Describe Yourself",
-                        contentPadding:
-                            const EdgeInsets.only(bottom: 5.0, top: 12.5),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ))),
-                Text("Tell us about your Hobbies",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 35, 34, 34),
-                        fontSize: 17,
-                        fontWeight: FontWeight.w600)),
-                TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: "Write your hobbies",
-                        contentPadding:
-                            const EdgeInsets.only(bottom: 5.0, top: 12.5),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ))),
-                Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: Center(
-                        child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          primary: Color.fromRGBO(8, 143, 143, 1)),
-                      onPressed: () {},
-                      child: Container(
-                        width: double.infinity,
-                        child: Center(
-                          child: Text(
-                            'Finish',
-                            style: TextStyle(fontSize: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Tell Us About Yourself",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 35, 34, 34),
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600)),
+                  TextField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: "Describe Yourself",
+                          contentPadding:
+                              const EdgeInsets.only(bottom: 5.0, top: 12.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ))),
+                  Text("Tell us about your Hobbies",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 35, 34, 34),
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600)),
+                  TextField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: "Write your hobbies",
+                          contentPadding:
+                              const EdgeInsets.only(bottom: 5.0, top: 12.5),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ))),
+                  Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Center(
+                          child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            primary: Color.fromRGBO(8, 143, 143, 1)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'start');
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          child: Center(
+                            child: Text(
+                              'Finish',
+                              style: TextStyle(fontSize: 20),
+                            ),
                           ),
                         ),
-                      ),
-                    ))),
-              ],
+                      ))),
+                ],
+              ),
             )));
   }
 }
